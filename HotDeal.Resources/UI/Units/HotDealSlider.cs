d971f8806986aace951c5f33ce9793c6f4991c9a
sersen28 <sersen28@daum.net>
2023-11-15 17:51:24 +0900

    Slider 구현

---
+++ HotDeal.Resources/UI/Units/HotDealSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HotDeal.Resources.UI.Units
+{
+	public class HotDealSlider : Slider
+	{
+		static HotDealSlider()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(HotDealSlider),
+				new FrameworkPropertyMetadata(typeof(HotDealSlider)));
+		}
+	}
+}
