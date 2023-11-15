3b55f48c148fff9ea15a6aaff974f868f71f0a76
sersen28 <sersen28@daum.net>
2023-11-15 15:50:19 +0900

    리스트 스크롤뷰어 구현

---
+++ HotDeal.Resources/UI/Units/ListScrollViewer.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HotDeal.Resources.UI.Units
+{
+	public class ListScrollViewer : ScrollViewer
+	{
+		static ListScrollViewer()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ListScrollViewer),
+				new FrameworkPropertyMetadata(typeof(ListScrollViewer)));
+		}
+	}
+}
