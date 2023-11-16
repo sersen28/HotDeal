956cf8137235f4f1bc75b9147d25592902ec3ced
sersen28 <sersen28@daum.net>
2023-11-16 13:33:33 +0900

    BooleanToVisibilityCoverter 추가

---
+++ HotDeal.Resources/Converters/BooleanToVisibilityCoverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HotDeal.Resources.Converters
+{
+	public class BooleanToVisibilityCoverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool ret)
+			{
+				return ret ? Visibility.Visible : Visibility.Collapsed;
+			}
+			else
+			{
+
+				return Visibility.Collapsed;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
