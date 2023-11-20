5e5ed1c02d835dbe970b0431f9beffb6040be68c
sersen28 <sersen28@daum.net>
2023-11-20 13:43:37 +0900

    메인메뉴 레이아웃 초안

---
+++ HotDeal.Resources/UI/Units/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HotDeal.Resources.UI.Units
+{
+	public class MenuButton : RadioButton
+	{
+		static MenuButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(MenuButton),
+				new FrameworkPropertyMetadata(typeof(MenuButton)));
+		}
+	}
+}
