5e6d56a8d85118fba2d719c120122e0e6ba7aedb
김태훈 <sersen28@daum.net>
2024-01-25 11:17:14 +0900

    TitleView 최대화시 잘림 문제 수정

---
+++ HotDeal.Resources/UI/Units/TitleCloseButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HotDeal.Resources.UI.Units
+{
+    public class TitleCloseButton : Button
+	{
+		public Thickness ContentMargin
+		{
+			get { return (Thickness)GetValue(ContentMarginProperty); }
+			set { SetValue(ContentMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty ContentMarginProperty =
+			DependencyProperty.Register("ContentMargin", typeof(Thickness),
+				typeof(TitleCloseButton));
+
+		static TitleCloseButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(TitleCloseButton),
+				new FrameworkPropertyMetadata(typeof(TitleCloseButton)));
+		}
+	}
+}
--- HotDeal/Views/TitleView.xaml.cs
+++ HotDeal/Views/TitleView.xaml.cs
@@ -1,12 +1,39 @@
-﻿using System.Windows.Controls;
+﻿using HotDeal.Items;
+using HotDeal.Resources.Constants;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace HotDeal.Views
 {
 	/// <summary>
 	/// Interaction logic for TitleView
 	/// </summary>
+	/// 
+
 	public partial class TitleView : UserControl
 	{
+		public TitleViewType Type
+		{
+			get { return (TitleViewType)GetValue(TypeProperty); }
+			set { SetValue(TypeProperty, value); }
+		}
+
+		public static readonly DependencyProperty TypeProperty =
+			DependencyProperty.Register("Type", typeof(TitleViewType),
+				typeof(TitleView));
+
+
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+
+		public static readonly DependencyProperty TitleProperty =
+			DependencyProperty.Register("Title", typeof(string),
+				typeof(TitleView));
+
+
 		public TitleView()
 		{
 			InitializeComponent();
